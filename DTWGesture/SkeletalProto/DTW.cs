621a7ed0eee673c012d02e41f902ac6cd6645839
David Nathan <20356245@student.uwa.edu.au>
2012-09-15 16:45:17 +0800

    Second Attempt at adding all the files

---
+++ DTWGesture/SkeletalProto/DTW.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Microsoft.Kinect;
+
+public class mdDTW
+{
+	public void DTW()
+	{
+
+	}
+}
