2286679603b88fc5339b0b65978751de495549a8
Patrick Thomas <pdthomas@y7mail.com>
2012-09-17 11:20:20 +0800

    Start - Stop
    Added start & stop buttons

--- Kinect Display/Project_v1.1/Project_v1.1/obj/x86/Debug/Project_v1.1_Content.g.i.cs
+++ Kinect Display/Project_v1.1/Project_v1.1/obj/x86/Debug/Project_v1.1_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/kinect_angle.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/stop.png")]
 
 
