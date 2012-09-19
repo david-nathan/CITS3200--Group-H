f6c4f6207733520272be00e4479c1bd172ebf1ab
Patrick Thomas <pdthomas@y7mail.com>
2012-09-19 11:29:00 +0800

    hope this works

--- DTWGesture/SkeletalProto/WriteSkeleton.cs
+++ DTWGesture/SkeletalProto/WriteSkeleton.cs
@@ -9,26 +9,38 @@ public class WriteSkeleton
 {
     
 
-	public void WriteSkeletonToFile(Skeleton skeleton, SkeletonFrame skeletonFrame, System.IO.StreamWriter file)
+	public float[] WriteSkeletonToFile(Skeleton skeleton, SkeletonFrame skeletonFrame, System.IO.StreamWriter file)
 	{
 
+        int count = 2; 
+            float[] dataLine = new float[63];
             System.Text.StringBuilder sb = new System.Text.StringBuilder(150);
-            sb.Append(skeletonFrame.FrameNumber.ToString());
+            string str_framenum = skeletonFrame.FrameNumber.ToString();
+            dataLine[0] = float.Parse(str_framenum);
+            sb.Append(str_framenum);
             sb.Append(" , ");
-            sb.Append(skeletonFrame.Timestamp.ToString());
+            string str_timestamp = skeletonFrame.Timestamp.ToString();
+            dataLine[1] = float.Parse(str_timestamp);
+            sb.Append(str_timestamp);
             sb.Append(" , ");
 
             foreach (Joint joint in skeleton.Joints){
-            
+
+                dataLine[count] = joint.Position.X;
                 sb.Append(joint.Position.X.ToString());
                 sb.Append(" , ");
+                dataLine[count + 1] = joint.Position.Y;
                 sb.Append(joint.Position.Y.ToString());
                 sb.Append(" , ");
+                dataLine[count + 2] = joint.Position.Z;
                 sb.Append(joint.Position.Z.ToString());
                 sb.Append(" , ");
+                count = count + 3;
             }
 
             string str = sb.ToString();
-            file.WriteLine(str);        
+         
+            file.WriteLine(str);
+            return dataLine;
 	}
 }
--- DTWGesture/SkeletalProto/obj/x86/Debug/SkeletalProto_Content.g.i.cs
+++ DTWGesture/SkeletalProto/obj/x86/Debug/SkeletalProto_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/status.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("data.sdf")]
 
 
