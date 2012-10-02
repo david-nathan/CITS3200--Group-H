73cf66f297a82b7958ca606261abb6179540c140
David Nathan <20356245@student.uwa.edu.au>
2012-10-02 21:55:04 +0800

    2nd Try: Added the DialogBox for RecordedGesture

---
+++ KinectWindows/Project_v1.1/Project_v1.1/RecordGestureDialog.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_v1._1
+{
+    /// <summary>
+    /// Interaction logic for RecordGestureDialog.xaml
+    /// </summary>
+    public partial class RecordGestureDialog : Window
+    {
+        public RecordGestureDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+    }
+
+
+}
